6a5572351a2f088c6513b78d2f7751dc6bce5dfb
DavidRovira18 <david.rovira01@estudiant.upf.edu>
2022-05-31 18:24:01 +0200

    some minor changes

--- main.py
+++ main.py
@@ -5,17 +5,19 @@ import interface as interface
 
 text = interface.run()
 keywords, synonyms = ts.query(text)
+print(synonyms)
 class_names, class_objects = db_classes.get_classes()
 
-for item in keywords:
+for item in synonyms:
     if item.lower() in class_names:
-        index = class_names.index(item.lower())
-        class_name = class_names[index].capitalize()
-        obj = class_objects[index]
-        class_ = getattr(db_classes, class_name)
-        instance = class_()
-        sounds = instance.sounds
-        queries = db.generate_queries(sounds)
-        previews = db.generate_previews(queries)
+            index = class_names.index(item.lower())
+            class_name = class_names[index].capitalize()
+            obj = class_objects[index]
+            class_ = getattr(db_classes, class_name)
+            instance = class_()
+            sounds = instance.sounds
+            queries = db.generate_queries(sounds)
+            print(queries)
+            #previews = db.generate_previews(queries)
 
 
